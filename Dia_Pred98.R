572231b4267b0f05137591babbf51ef8e94bd7a4
Ginchan99 <pg21tushti.sinha@isbmb.ac.in>
2022-12-12 15:51:20 +0530

    first commit

--- Dia_Pred98.R
+++ Dia_Pred98.R
@@ -19,7 +19,8 @@ diapredDF$diapred = diapredDF$pred>0.5
 
 #confusion matrix -  categorize the predictions against the actual values
 
-cf = table(diapredDF$Outcome,diapredDF$diapred)# above 0.5 will be TRUE and below will be FALSE as per prediction values
+# >0.5 will be TRUE and <0.5 will be FALSE 
+cf = table(diapredDF$Outcome,diapredDF$diapred)
 
 cf = as.data.frame.matrix(table(diapredDF$Outcome,diapredDF$diapred))# to check if the function is a data frame
 
@@ -27,5 +28,6 @@ View(cf)
 accuracy = (96+32)/(96+12+28+32)
 Truepositive = (96+32)/(96+12+28+32)
 Truenegative = (32)/(96+12+28+32)
-Falsepositive = (12)/(96+12+28+32) # also called type1 error
-Falsenegative = (28)/(96+12+28+32) # also called type2 error
+Falsepositive = (12)/(96+12+28+32) 
+Falsenegative = (28)/(96+12+28+32) 
+
